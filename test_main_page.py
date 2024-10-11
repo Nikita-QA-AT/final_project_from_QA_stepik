410e735d1d395c40f479b6b501da5c7eedf73516
Nikita-QA-AT <olkin.nikita777@gmail.com>
2024-10-11 17:16:31 +0300

    peredelali test_main_page_v_stil_PageObject

--- test_main_page.py
+++ test_main_page.py
@@ -7,8 +7,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from .pages.main_page import MainPage
 
-link = "http://selenium1py.pythonanywhere.com/"
+
 
 def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
@@ -16,7 +17,7 @@ def go_to_login_page(browser):
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
-    time.sleep(5)
-    print()
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина

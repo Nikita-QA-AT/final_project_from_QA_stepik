7895728f834cd5c4ea3aa6bbd2df977abc73ef3a
Nikita-QA-AT <olkin.nikita777@gmail.com>
2024-10-26 22:24:20 +0300

    realisovali proverki should_be_login_url, should_be_login_form, should_be_register_form

--- test_main_page.py
+++ test_main_page.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from .pages.main_page import MainPage
-
+from .pages.login_page import LoginPage
 
 
 def go_to_login_page(browser):
@@ -29,3 +29,11 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+    
+    
+def test_guest_can_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    
+    page.should_be_login_page()

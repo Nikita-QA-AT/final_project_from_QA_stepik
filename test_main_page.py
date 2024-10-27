ce0cf174deffa58969735c646f001b3bf2a06f17
Nikita-QA-AT <olkin.nikita777@gmail.com>
2024-10-27 21:57:57 +0300

    novaya_proverka_test_test_guest_can_add_product_to_basket

--- test_main_page.py
+++ test_main_page.py
@@ -11,6 +11,7 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 
+
 def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
@@ -19,8 +20,10 @@ def go_to_login_page(browser):
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    page.open()        # открываем страницу
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()         # выполняем метод страницы — переходим на страницу логина
     
     
     
---
+++ test_product_page.py
@@ -0,0 +1,29 @@
+import pytest
+import time
+import math
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+from .pages.product_page import ProductPage
+
+
+
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
+
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()        # открываем страницу
+    time.sleep(5)
+    page.add_to_cart() # через Page Object вызвал метод add_to_cart, который нажимает кнопку "Добавить в корзину"
+    time.sleep(1)
+    page.solve_quiz_and_get_code()
+    time.sleep(600)

d07f20c707dce8fcb12bf99f184c8dd8d8b38f63
Nikita-QA-AT <olkin.nikita777@gmail.com>
2024-10-30 21:52:20 +0300

    realisoval test test_guest_cant_see_product_in_basket_opened_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -10,6 +10,7 @@ from selenium.common.exceptions import TimeoutException
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 
 
 
@@ -90,3 +91,15 @@ def est_guest_can_go_to_login_page_from_product_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()         # выполняем метод страницы — переходим на страницу логина
     print("успешно проверили, что на странице логина есть необходимые элементы и ссылки")
+    
+    
+    
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()    # открываем страницу 
+    # перешли на страницу корзины (теперь можно применять методы из basket_page.py)
+    page.go_to_basket_page()
+    time.sleep(5)
+    page = BasketPage(browser, link)
+    page.should_be_message_vasha_korzina_pysta()

85c39d871bbdeb3bd2b69047c4ba10ed6c0e0bc5
Никита Олькин <nolkin@ad.local>
2024-10-30 08:47:36 +0300

    realisoval metod test_guest_can_go_to_login_page_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -72,8 +72,21 @@ def est_message_disappeared_after_adding_product_to_basket(browser):
     page.should_element_dolshen_ischezat()
 
 
-def test_guest_should_see_login_link_on_product_page(browser):
+def est_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
+    
+    
+    
+    
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()        # открываем страницу
+    page.go_to_login_page()
+    print("успешно перешли на страницу логина")
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()         # выполняем метод страницы — переходим на страницу логина
+    print("успешно проверили, что на странице логина есть необходимые элементы и ссылки")

f977e668f84445fef11d2481a0eb25b218512ffa
Никита Олькин <nolkin@ad.local>
2024-10-31 13:36:25 +0300

    dobavili fixture setup s registraziyei polzovatelya

--- pages/locators.py
+++ pages/locators.py
@@ -8,6 +8,12 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTER_EMAIL_INPUT = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD_INPUT = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_PODTVERSHD_PASSWORD_INPUT = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "#register_form .btn-primary.btn-lg")
+    
+    
     
 class ProductPageLocators():
     ADD_TO_CART_SELECTOR = (By.CSS_SELECTOR, ".btn-add-to-basket")
@@ -22,6 +28,7 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     KNOPKA_POSMOTRET_KORSINY_LINK = (By.CSS_SELECTOR, "span.btn-group a.btn.btn-default")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
     
     
     
--- test_product_page.py
+++ test_product_page.py
@@ -108,6 +108,26 @@ def est_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     
 @pytest.mark.user_add_to_basket
 class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        # Откроем страницу регистрации
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        
+        # Зарегистрируем нового пользователя
+        email = str(time.time()) + "@fakemail.org"
+        password = "strong_password"
+        # вызываем метод для регистрации нового пользователя
+        page.register_new_user(email, password)
+        print("успешно зарегистрировали нового пользователя")
+        
+        # Проверим, что пользователь авторизован
+        page.should_be_authorized_user()  
+        print("успешно проверили, что польователь авторизован")        
+       
+    
+    
     def test_user_cant_see_success_message(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 

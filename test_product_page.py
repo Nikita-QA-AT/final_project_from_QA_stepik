891c0284ada418237538c0dcaca69de28654b5ed
Nikita-QA-AT <olkin.nikita777@gmail.com>
2024-11-04 14:04:11 +0300

    priveli v poryadok sintaksis

--- test_product_page.py
+++ test_product_page.py
@@ -106,7 +106,7 @@ def test_guest_can_add_product_to_basket(browser):
     page.solve_quiz_and_get_code()
     page.should_be_message_about_adding()
     page.should_be_message_basket_total()
-    print("успешно прошел тест test_user_can_add_product_to_basket")
+    print("успешно прошел тест test_guest_can_add_product_to_basket")
         
         
 @pytest.mark.need_review         
